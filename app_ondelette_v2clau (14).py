f3284ddacae823b71cdacec36ff18be4ce40ea5f
ZARAVITAH <scarlzaravita@gmail.com>
2025-06-02 17:01:51 +0100

    Update app_ondelette_v2clau (14).py

--- app_ondelette_v2clau (14).py
+++ app_ondelette_v2clau (14).py
@@ -917,12 +917,16 @@ def main():
                             )
                             
                             # Affichage des graphiques
+                            st.plotly_chart(fig_time, use_container_width=True)
+                            st.plotly_chart(fig_env, use_container_width=True)
+                            #---------------------en colonne
+                            '''
                             col1, col2 = st.columns(2)
                             with col1:
                                 st.plotly_chart(fig_time, use_container_width=True)
                             with col2:
                                 st.plotly_chart(fig_env, use_container_width=True)
-                            
+                            '''
                             # Diagnostic automatique
                             st.subheader("🔍 Diagnostic Automatisé")
                             
