a2da4ee7518e32125d25ecddf3281f6714d6ae2d
ZARAVITAH <scarlzaravita@gmail.com>
2025-06-03 18:36:19 +0100

    Update app_ondelette_v2clau (14).py

--- app_ondelette_v2clau (14).py
+++ app_ondelette_v2clau (14).py
@@ -708,6 +708,7 @@ def main():
                         title="Spectre FFT du Signal Traité (Fenêtre de Hanning)",
                         xaxis_title="Fréquence (Hz)",
                         yaxis_title="Amplitude",
+                        xaxis_range=[0.01, np.max(fft_freq)/2],   #test sur 2
                         yaxis=dict(
                                    range=[0, 1.1 * np.max(fft_amp[fft_freq > 0.01])],  # Ignore les très basses fréquences--------------------------------------
                                    autorange=False
